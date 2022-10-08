1020c6f601635b14b62015d0939ed27609a59827
Darshit4511 <kakadiyadarshit0@gmail.com>
2022-10-08 10:16:38 +0530

    url changed for form post

--- src/components/MissingPersonForm.jsx
+++ src/components/MissingPersonForm.jsx
@@ -13,7 +13,7 @@ const Form = () => {
         type: "",
     });
     const createMissingPerson = () => {
-        axios.post("http://localhost:4000/MissingPerson", MissingPerson).then(() => {
+        axios.post("https://criminal-profiling-server.herokuapp.com/", MissingPerson).then(() => {
             window.location.reload(false);
         })
     };
